a59ffaa6cecad4fbbbd01a785142641882dc6240
lauanderson-rael <lauanderson38@gmail.com>
2025-05-07 16:01:50 -0300

    ok

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,8 @@ function authToken(req, res, next) {
   const authHeader = req.headers['authorization']
   const token = authHeader && authHeader.split(' ')[1]
 
-  if (!token) return res.status(401).json({ message: 'Token não fornecido!' });
+  //if (!token) return res.status(401).json({ message: 'Token não fornecido!' });
+  if (!token) return res.redirect('unauthorized.html')
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) return res.status(401).json({ message: 'Token inválido!' });
--- server.js
+++ server.js
@@ -4,7 +4,6 @@ require('dotenv').config()
 const path = require('path')
 const { authToken } = require('./middleware/auth.js');
 
-
 const app = express()
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public'))) // servir frontend
@@ -12,20 +11,27 @@ app.use(express.static(path.join(__dirname, 'public'))) // servir frontend
 const PORT = 3000
 const USERS = [{ id: 1, username: 'lauan', password: '1234' }]
 
-// rota de login
+
+// rota de login - realizar login
 app.post('/login', (req, res) => {
   const { username, password } = req.body
   const user = USERS.find(u => u.username === username && u.password === password)
 
   if (!user) return res.status(401).json({ message: 'Credenciais inválidas!' })
-  const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' })
+  const token = jwt.sign({ username: user.username }, process.env.JWT_SECRET, { expiresIn: '1m' })
   res.json({ token })
 })
 
- // Rota de API para testar conteúdo da área protegida
+ // Rota de API para retornar dados do usuario
  app.get('/protected', authToken, (req, res) => {
+   console.log(req.user); 
    res.json({ message: `Olá, ${req.user.username}. Você está autenticado!` });
  });
+
+ // rota que retorna pagina login.html
+ app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html')); // ou redirect()
+ })
 
 app.listen(PORT, () => {
   console.log(`App rodando na porta ${PORT}`)
